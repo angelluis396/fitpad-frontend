cf7b1f53fecd1d27275ca4aaf6834a4a50bb455d
Angel Alicea <ala31996@gmail.com>
2020-01-02 15:57:51 -0500

    added token service,auth service,  and register service

--- src/Components/LoginRegister/Register.js
+++ src/Components/LoginRegister/Register.js
@@ -1,47 +1,83 @@
 import React, { Component } from 'react'
 import './Register.css'
+import AuthApiService from '../../Services/auth-api-service'
 
 
 export class Register extends Component {
-  state = {
-    username:'',
-    email: '',
-    password: '',
-  }
-  onChange = (e) => {
-    const value= e.target.checked === undefined? e.target.value : e.target.checked;
-    this.setState({
-      [e.target.name]: value,
-    })
-  }
+ static defaultProps = {
+   onRegistrationSuccess: () => {}
+ }
+
+ state = {error: null}
+
+ handleSubmit = event => {
+  event.preventDefault();
+  const {first_name, last_name, user_name, user_email, password} = event.target;
+
+  this.setState({error:null})
+  AuthApiService.postUser({
+    first_name: first_name.value,
+    last_name: last_name.value,
+    user_name: user_name.value,
+    user_email: user_email.value,
+    password: password.value
+  })
+  .then(user => {
+    first_name.value=''
+    last_name.value=''
+    user_name.value=''
+    user_email.value=''
+    password.value=''
+    this.props.onRegistrationSuccess();
+  })
+  .catch(res => {
+    this.setState({error: res.error});
+  });
+ };
+
   render() {
+    const {error}=this.state;
     return (
       <div>
-        <form className="registerForm">
+        <form className="registerForm" onSubmit={this.handleSubmit}>
 
-          <div className="FormField">
-            <label className="formLabel" htmlFor="username"> Username: </label>
-            <input type="text" id="username" className="formInput" placeholder="Your Username" name="username"/>
+          <div role='alert'>
+            {error && <p className='red'>{error}</p>}
           </div>
 
           <div className="FormField">
-            <label className="formLabel" htmlFor="email"> Email: </label>
-            <input type="email" id="email" className="formInput" placeholder="Your Email" name="email"/>  
+            <label className="formLabel" htmlFor="first_name"> First Name:<required/> </label>
+            <input required type="text" id="first_name" className="formInput" placeholder="First Name" name="first_name"/>
           </div>
 
           <div className="FormField">
-            <label className="formLabel" htmlFor="password"> Password: </label>
-            <input type="text" id="password" className="formInput" placeholder="Password" name="password"/>
+            <label className="formLabel" htmlFor="last_name"> Last Name:<required/> </label>
+            <input  required type="text" id="last_name" className="formInput" placeholder="Last Name" name="last_name"/>
+          </div>
+
+          <div className="FormField">
+            <label className="formLabel" htmlFor="username"> Username:<required/> </label>
+            <input required type="text" id="user_name" className="formInput" placeholder="Your Username" name="user_name"/>
+          </div>
+
+          <div className="FormField">
+            <label className="formLabel" htmlFor="user_email"> Email:<required/> </label>
+            <input required type="email" id="user_email" className="formInput" placeholder="Your Email" name="user_email"/>  
+          </div>
+
+          <div className="FormField">
+            <label className="formLabel" htmlFor="password"> Password:<required/> </label>
+            <input required type="text" id="password" className="formInput" placeholder="Password" name="password"/>
           </div>
 
           <div className="FormField">
             <label className="formCheckBox">
-              <input className="formCheckBox" type="checkbox" name="hasAgreed" /> I Agree to the Terms and Conditions
+              <input required className="formCheckBox" type="checkbox" name="hasAgreed" /> I Agree to the Terms and Conditions
             </label>
           </div>
 
           <div className="FormField">
-            <button className="formButton"> Sign Up </button>
+            <button type="submit" className="formButton"> Register </button>
           </div>
          
         </form>
--- src/config.js
+++ src/config.js
@@ -1,1 +1,5 @@
-export default { API_ENDPOINT: "http://localhost:8000" }
+export default { 
+  API_ENDPOINT: "http://localhost:8000",
+  TOKEN_KEY: "fitpad-client-auth-token" 
+
+}
